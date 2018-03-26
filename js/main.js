a3710bd522d57bb193ba9580b90a266e3e8cd550
曹 知霖 <wufeisou@gmail.com>
2018-03-26 15:41:37 +0900

    stage update

--- js/main.js
+++ js/main.js
@@ -5,23 +5,18 @@ var ctx;
 var x = 256;
 var y = 200;
 var up = false;
-var stage;
 var pattern = 3;
-var stageX = 492;
-var stageY = 200;
-var rand;
 
 //main
 window.onload = function() {
     var counter = 0;
-    var upcount = 0;
     var i,j;
-
+    
     //スクリーン初期化
     screenCanvas = document.getElementById('screen');
     screenCanvas.width = 512;
     screenCanvas.height = 256;
-
+    
     //2dコンテキスト
     ctx = screenCanvas.getContext('2d');
     
@@ -30,81 +25,47 @@ window.onload = function() {
     
     //エレメント関連
     info = document.getElementById('info');
-
-    //ステージ
-    var stage = new Array();
-    for(let k = 0; k < stage.length; k++){
-        stage[k] = new stage();
+    
+    //ランダム地形を配列に保存
+    var rndLnd = [20];
+    for (let i = 0; i < rndLnd.length; i++) {
+        var drwRnd = (Math.random()*9) + 1;
+        rndLnd[i] = drwRnd;        
     }
-
+    
     (function(){
         counter++;
         //HTML更新
         info.innerHTML = 'SCORE ' + counter;
-
+        
         // screenクリア 
         ctx.clearRect(0, 0, screenCanvas.width, screenCanvas.height);
-
+        
         var pattern = new Array(3);
         for(i = 0; i < pattern.length; i++){
             pattern[i] = (Math.random() * 3) + 1; 
         }
-
-        /*//ジャンプ
-        if(up == true){
-            y--;
-            var huga = 0;
-            var hoge = setInterval(function() {
-                huga++;
-                y++;
-                //終了条件
-                if (huga == 1) {
-                    clearInterval(hoge);
-                }
-            }, 100);
-            // while(upcount<50) {
-            //     upcount++;
-            // }
-            // do{
-            //     y++;
-            //     upcount--;
-            // }while(upcount == -1);{
-            //     upcount++
-            //     up = false;
-            // }
-        }
-*/
-
+        
+        
         //パス設定開始
         ctx.beginPath();
-
+        
         //キャラのパス設定
         ctx.fillRect(x,y,10,10);
         
         //色を設定
         ctx.fillStyle = 'rgba(0, 0, 255, 0.75)';
         ctx.fill();
-
+        
         ctx.closePath;
-
-        //ステージ
-        while(i<30000){
-            rand = (Math.random() * 50) + 1;
-            ctx.beginPath();
-            ctx.fillRect(
-                stage[i] = stageX-=20,
-                stage[i] = stageY + rand,
-                20,256
-            );
-            // console.log(i);
-            ctx.closePath();
-            i++;
-        }
+        
+        //ステージ 
+        updateStage(rndLnd);
 
         //色を設定
         ctx.fillStyle = 'red';
         ctx.fill();
-
+        
         if(run){setTimeout(arguments.callee,fps);}
     })();
 };
@@ -117,9 +78,57 @@ function keyDown(event){
     // →キー
     if(key === 39){x+=5; console.log('x');}
     // ↑キー
-    if(key === 38){up = true; console.log('y');
+    if(key === 38){up = true; console.log('y');}
+}
+
+
+//ステージ
+var stage = 0;
+var stageX = 512;
+var stageY = 210;
+// let rand = (Math.random() * 50) + 1;
+function drawRoad(/*rand*/) {
+    ctx.beginPath();
+    ctx.fillRect(
+        stage = stageX -= 20,
+        stage = stageY/* + rand*/,
+        20,256
+    );
+    ctx.closePath();
+}
+
+function vanishRoad() {
+    ctx.beginPath();
+    ctx.fillRect(
+        stage = stageX -= 20,
+        stage = 256,
+        20,256
+    );
+    ctx.closePath();    
+}
+
+function drawStage(drw_nmb) {
+    for (let num = 0; num < drw_nmb; num++) {        
+        drawRoad();
+    }
+    vanishRoad();
+    if(stageX==0) {
+        stageX=512;
     }
 }
 
-function jump(j){
+var stageX_speed = 20;
+function moveStage() {
+    stageX += stageX_speed;
+}
+
+function generateStage(rndLnd) {
+    for (let i = 0; i < rndLnd.length; i++) {        
+        drawStage(rndLnd[i]);
+    }
+}
+
+function updateStage(rndLnd) {
+    generateStage(rndLnd);
+    moveStage();
 }
