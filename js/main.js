348cdeeb5a219ed6d4cc3849b28aef715c37281a
曹 知霖 <wufeisou@gmail.com>
2018-03-26 16:40:09 +0900

    update

--- js/main.js
+++ js/main.js
@@ -43,21 +43,11 @@ window.onload = function() {
         // screenクリア 
         ctx.clearRect(0, 0, screenCanvas.width, screenCanvas.height);
 
-<<<<<<< HEAD
-        //カウンター
-        //経過時間と開始判定
-        if(start == true){
-            do{
-                y++;
-                switch(true){
-                    case (x > 0 && x < 512 && y < 256):
-=======
         //経過時間と開始判定
         if(start == true){
             do{
                 switch(true){
                     case (x > 0 && x < 600 && y > 0):
->>>>>>> test
                         counter++
                         message = counter;
                         break;
@@ -72,10 +62,7 @@ window.onload = function() {
                         y=540;
                         break;
 
-<<<<<<< HEAD
-=======
                     // First Start
->>>>>>> test
                     case start == true:
                         counter=0;
                         //自機の初期位置
